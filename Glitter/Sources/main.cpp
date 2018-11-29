d3b490fcec48dcae2b5604921e7b81bfdf64e075
Piasy <xz4215@gmail.com>
2018-11-29 22:06:02 +0800

    rectangle!
    get real frame buffer size and set it to view port on macOS.

--- Glitter/Sources/main.cpp
+++ Glitter/Sources/main.cpp
@@ -10,6 +10,8 @@
 #include <cstdlib>
 #include <iostream>
 
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+
 int main(int argc, char * argv[]) {
 
     // Load GLFW and Create a Window
@@ -17,8 +19,12 @@ int main(int argc, char * argv[]) {
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    
+#ifdef __APPLE__
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+#endif
+    
+    //glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
     auto mWindow = glfwCreateWindow(mWidth, mHeight, "OpenGL", nullptr, nullptr);
 
     // Check for Valid Context
@@ -29,6 +35,7 @@ int main(int argc, char * argv[]) {
 
     // Create Context and Load OpenGL Functions
     glfwMakeContextCurrent(mWindow);
+    glfwSetFramebufferSizeCallback(mWindow, framebuffer_size_callback);
     gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
     fprintf(stderr, "OpenGL %s\n", glGetString(GL_VERSION));
 
@@ -84,11 +91,15 @@ int main(int argc, char * argv[]) {
 
     glDeleteShader(vertex_shader);
     glDeleteShader(fragment_shader);
-    
+
     GLfloat vertices[] = {
-        -0.5f, -0.5f, 0.0f,
-        0.5f, -0.5f, 0.0f,
-        0.0f,  0.5f, 0.0f
+        0.5f,  0.5f,  0.0f,  // top right
+        0.5f, -0.5f,  0.0f,  // bottom right
+        -0.5f, -0.5f, 0.0f,  // bottom left
+        -0.5f, 0.5f,  0.0f,  // top left
+    };
+    GLuint indices[] = {
+        0, 1, 3, 1, 2, 3,
     };
 
     GLuint vao;
@@ -100,14 +111,15 @@ int main(int argc, char * argv[]) {
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+    GLuint ebo;
+    glGenBuffers(1, &ebo);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices,
+                 GL_STATIC_DRAW);
+
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float),
                           (void*)0);
     glEnableVertexAttribArray(0);
-
-    // note that this is allowed, the call to glVertexAttribPointer registered
-    // VBO as the vertex attribute's bound vertex buffer object so afterwards we
-    // can safely unbind
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
 
     // You can unbind the VAO afterwards so other VAO calls won't accidentally
     // modify this VAO, but this rarely happens. Modifying other
@@ -115,8 +127,19 @@ int main(int argc, char * argv[]) {
     // unbind VAOs (nor VBOs) when it's not directly necessary.
     glBindVertexArray(0);
 
+    // note that this is allowed, the call to glVertexAttribPointer registered
+    // VBO as the vertex attribute's bound vertex buffer object so afterwards we
+    // can safely unbind
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
     glUseProgram(program);
-    glViewport(0, 0, mWidth, mHeight);
+
+    GLint fboWidth, fboHeight;
+    glfwGetFramebufferSize(mWindow, &fboWidth, &fboHeight);
+    glViewport(0, 0, fboWidth, fboHeight);
+    glClearColor(0.25f, 0.25f, 0.25f, 1.0f);
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
     // Rendering Loop
     while (glfwWindowShouldClose(mWindow) == false) {
@@ -125,14 +148,18 @@ int main(int argc, char * argv[]) {
         }
 
         // why can't view port here?
-        //glViewport(0, 0, mWidth, mHeight);
+        // glViewport(0, 0, mWidth, mHeight);
+        // With HiDPI monitors, it can happen that the framebuffer size is not
+        // the same as the window pixel size.
+        // https://stackoverflow.com/q/53538648/3077508#comment93945404_53538648
+        // glViewport(0, 0, fboWidth, fboHeight);
 
         // Background Fill Color
-        glClearColor(0.25f, 0.25f, 0.25f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
         glBindVertexArray(vao);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glBindVertexArray(0);
 
         // Flip Buffers and Draw
         glfwSwapBuffers(mWindow);
@@ -140,4 +167,12 @@ int main(int argc, char * argv[]) {
     }
     glfwTerminate();
     return EXIT_SUCCESS;
+}
+
+// glfw: whenever the window size changed (by OS or user resize) this callback function executes
+// ---------------------------------------------------------------------------------------------
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+    // make sure the viewport matches the new window dimensions; note that width and
+    // height will be significantly larger than specified on retina displays.
+    glViewport(0, 0, width, height);
 }
