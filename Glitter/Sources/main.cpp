ee8e7b354e37a86f1e1375272ef1130a053cb4b0
Piasy <xz4215@gmail.com>
2018-11-30 00:09:02 +0800

    two programs, two triangles in different color

--- Glitter/Sources/main.cpp
+++ Glitter/Sources/main.cpp
@@ -10,6 +10,30 @@
 #include <cstdlib>
 #include <iostream>
 
+const GLchar* fragment_shader_src1 =
+    "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "void main()\n"
+    "{\n"
+    "    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+    "}";
+
+const GLchar* fragment_shader_src2 =
+    "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "void main()\n"
+    "{\n"
+    "    FragColor = vec4(1.0f, 1.0f, 0.0f, 1.0f);\n"
+    "}";
+
+const GLchar* vertex_shader_src =
+    "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main()\n"
+    "{\n"
+    "    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "}";
+
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 
 int main(int argc, char * argv[]) {
@@ -39,13 +63,6 @@ int main(int argc, char * argv[]) {
     gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
     fprintf(stderr, "OpenGL %s\n", glGetString(GL_VERSION));
 
-    const GLchar* vertex_shader_src =
-        "#version 330 core\n"
-        "layout (location = 0) in vec3 aPos;\n"
-        "void main()\n"
-        "{\n"
-        "    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-        "}";
     GLuint vertex_shader = glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(vertex_shader, 1, &vertex_shader_src, nullptr);
     glCompileShader(vertex_shader);
@@ -59,38 +76,55 @@ int main(int argc, char * argv[]) {
                   << infoLog << std::endl;
     }
 
-    const GLchar* fragment_shader_src =
-        "#version 330 core\n"
-        "out vec4 FragColor;\n"
-        "void main()\n"
-        "{\n"
-        "    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-        "}";
-    GLuint fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragment_shader, 1, &fragment_shader_src, nullptr);
-    glCompileShader(fragment_shader);
+    GLuint fragment_shader1 = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragment_shader1, 1, &fragment_shader_src1, nullptr);
+    glCompileShader(fragment_shader1);
 
-    glGetShaderiv(fragment_shader, GL_COMPILE_STATUS, &success);
+    glGetShaderiv(fragment_shader1, GL_COMPILE_STATUS, &success);
     if (!success) {
-        glGetShaderInfoLog(fragment_shader, 512, NULL, infoLog);
+        glGetShaderInfoLog(fragment_shader1, 512, NULL, infoLog);
         std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n"
                   << infoLog << std::endl;
     }
 
-    GLuint program = glCreateProgram();
-    glAttachShader(program, vertex_shader);
-    glAttachShader(program, fragment_shader);
-    glLinkProgram(program);
+    GLuint program1 = glCreateProgram();
+    glAttachShader(program1, vertex_shader);
+    glAttachShader(program1, fragment_shader1);
+    glLinkProgram(program1);
 
-    glGetProgramiv(program, GL_LINK_STATUS, &success);
+    glGetProgramiv(program1, GL_LINK_STATUS, &success);
     if (!success) {
-        glGetProgramInfoLog(program, 512, NULL, infoLog);
+        glGetProgramInfoLog(program1, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n"
+                  << infoLog << std::endl;
+    }
+
+    GLuint fragment_shader2 = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragment_shader2, 1, &fragment_shader_src2, nullptr);
+    glCompileShader(fragment_shader2);
+
+    glGetShaderiv(fragment_shader2, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        glGetShaderInfoLog(fragment_shader2, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n"
+                  << infoLog << std::endl;
+    }
+
+    GLuint program2 = glCreateProgram();
+    glAttachShader(program2, vertex_shader);
+    glAttachShader(program2, fragment_shader2);
+    glLinkProgram(program2);
+
+    glGetProgramiv(program2, GL_LINK_STATUS, &success);
+    if (!success) {
+        glGetProgramInfoLog(program2, 512, NULL, infoLog);
         std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n"
                   << infoLog << std::endl;
     }
 
     glDeleteShader(vertex_shader);
-    glDeleteShader(fragment_shader);
+    glDeleteShader(fragment_shader1);
+    glDeleteShader(fragment_shader2);
 
     GLfloat vertices[] = {
         0.5f,  0.5f,  0.0f,  // top right
@@ -98,68 +132,83 @@ int main(int argc, char * argv[]) {
         -0.5f, -0.5f, 0.0f,  // bottom left
         -0.5f, 0.5f,  0.0f,  // top left
     };
-    GLuint indices[] = {
-        0, 1, 3, 1, 2, 3,
+    GLuint indices1[] = {
+        0, 1, 3,
+    };
+    GLuint indices2[] = {
+        1, 2, 3,
     };
 
-    GLuint vao;
-    glGenVertexArrays(1, &vao);
-    glBindVertexArray(vao);
+    GLuint vao1;
+    glGenVertexArrays(1, &vao1);
+    glBindVertexArray(vao1);
 
     GLuint vbo;
     glGenBuffers(1, &vbo);
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    GLuint ebo;
-    glGenBuffers(1, &ebo);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices,
+    GLuint ebo1;
+    glGenBuffers(1, &ebo1);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo1);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices1), indices1,
                  GL_STATIC_DRAW);
 
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float),
                           (void*)0);
     glEnableVertexAttribArray(0);
 
-    // You can unbind the VAO afterwards so other VAO calls won't accidentally
-    // modify this VAO, but this rarely happens. Modifying other
-    // VAOs requires a call to glBindVertexArray anyways so we generally don't
-    // unbind VAOs (nor VBOs) when it's not directly necessary.
     glBindVertexArray(0);
 
-    // note that this is allowed, the call to glVertexAttribPointer registered
-    // VBO as the vertex attribute's bound vertex buffer object so afterwards we
-    // can safely unbind
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
-    glUseProgram(program);
+    GLuint vao2;
+    glGenVertexArrays(1, &vao2);
+    glBindVertexArray(vao2);
+
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+
+    GLuint ebo2;
+    glGenBuffers(1, &ebo2);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo2);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices2), indices2,
+                 GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float),
+                          (void*)0);
+    glEnableVertexAttribArray(0);
+
+    glBindVertexArray(0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
     GLint fboWidth, fboHeight;
     glfwGetFramebufferSize(mWindow, &fboWidth, &fboHeight);
     glViewport(0, 0, fboWidth, fboHeight);
     glClearColor(0.25f, 0.25f, 0.25f, 1.0f);
-    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
     // Rendering Loop
     while (glfwWindowShouldClose(mWindow) == false) {
         if (glfwGetKey(mWindow, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
             glfwSetWindowShouldClose(mWindow, true);
         }
-
-        // why can't view port here?
-        // glViewport(0, 0, mWidth, mHeight);
-        // With HiDPI monitors, it can happen that the framebuffer size is not
-        // the same as the window pixel size.
-        // https://stackoverflow.com/q/53538648/3077508#comment93945404_53538648
-        // glViewport(0, 0, fboWidth, fboHeight);
-
-        // Background Fill Color
+        
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glBindVertexArray(vao);
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glUseProgram(program1);
+        glBindVertexArray(vao1);
+        glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
         glBindVertexArray(0);
+        
+        glUseProgram(program2);
+        glBindVertexArray(vao2);
+        glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
+        glBindVertexArray(0);
+        
+        glUseProgram(0);
 
         // Flip Buffers and Draw
         glfwSwapBuffers(mWindow);
